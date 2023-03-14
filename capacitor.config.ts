9347b24b55fa68532d139115891e90213a0e239b
Ken Sodemann <ken@ionic.io>
2023-03-14 07:50:31 -0500

    step(two): add native platforms

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'tea-taster',
+  appId: 'com.kensodemann.teataster',
+  appName: 'Tea Taster',
   webDir: 'www',
   bundledWebRuntime: false,
 };
